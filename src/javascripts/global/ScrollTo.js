01a2508ac592b27b77eec284de99a6055727aa3f
Remi <remi.shergold@gmail.com>
2016-03-18 16:04:13 +0000

    fixed scrollTo on firefox

--- src/javascripts/global/ScrollTo.js
+++ src/javascripts/global/ScrollTo.js
@@ -8,16 +8,11 @@ function scroll_to(location) {
   }
 
   const duration = 500, 
-    start_pos = document.body.scrollTop,
+    start_pos = window.scrollY,
     end_pos = element.offsetTop,
     distance = end_pos - start_pos,
     start_time = Date.now(),
     end_time = start_time + duration;
-  
-  const requestAnimFrame = (function(){
-    //TODO i'm only targeting new browsers so how much of this do I need?
-    return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 1000 / 60); };
-  })();
   
   var animate_frame = function() {
 
@@ -25,12 +20,12 @@ function scroll_to(location) {
       percent = (now - start_time) / duration,
       ease_in = percent * percent,
       new_top = start_pos + (distance * ease_in);
-    document.body.scrollTop = new_top;
-    
+
+    window.scrollTo(0, new_top)
     if (now >= end_time) {
-      document.body.scrollTop = end_pos;
+      window.scrollTo(0, end_pos)
     } else {
-      requestAnimFrame(animate_frame);
+      requestAnimationFrame(animate_frame);
     }
   };
   
